007deb98e92c92255a1bfed7928a2a6626a1919a
CHamburr <cenhan@outlook.com>
2019-05-21 20:21:03 +0800

    Add miscellaneous commands

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -7,15 +7,94 @@ class Miscellaneous(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def say_permissions(self, ctx, member, channel):
+        permissions = channel.permissions_for(member)
+        embed = discord.Embed(
+            title=f"Permissions for {member.name}#{member.discriminator}",
+            color=self.bot.primary_colour,
+        )
+        allowed, denied = [], []
+        for name, value in permissions:
+            name = name.replace('_', ' ').replace('guild', 'server').title()
+            if value:
+                allowed.append(name)
+            else:
+                denied.append(name)
+
+        embed.add_field(name='Allowed', value='\n'.join(allowed))
+        embed.add_field(name='Denied', value='\n'.join(denied))
+        await ctx.send(embed=embed)
+
+    @commands.has_permissions(administrator=True)
     @commands.guild_only()
     @commands.command(
-        description="Go to sleep.",
-        usage="sleep",
+        description="Shows a member's permission in a channel when specified.",
+        usage="permissions [member] [channel]"
     )
-    async def sleep(self, ctx):
-        async with ctx.channel.typing():
-            await asyncio.sleep(5)
-            await ctx.send("Slept, done.")
+    async def permissions(self, ctx, member: discord.Member = None, channel: discord.TextChannel = None):
+        channel = channel or ctx.channel
+        if member is None:
+            member = ctx.author
+        await self.say_permissions(ctx, member, channel)
+
+    @commands.has_permissions(administrator=True)
+    @commands.guild_only()
+    @commands.command(
+        description="Shows the bot's permissions in the current or specified channel.",
+        usage="botpermissions [channel]"
+    )
+    async def botpermissions(self, ctx, *, channel: discord.TextChannel = None):
+        channel = channel or ctx.channel
+        member = ctx.guild.me
+        await self.say_permissions(ctx, member, channel)
+
+    @commands.guild_only()
+    @commands.command(
+        description="Shows some information about yourself or the member you specified.",
+        usage="userinfo [member]",
+    )
+    async def userinfo(self, ctx, *, member: discord.Member = None):
+        if member is None:
+            member = ctx.author
+        embed = discord.Embed(
+            title=f"User Information for {member.name}#{member.discriminator}",
+            color=self.bot.primary_colour,
+        )
+        roles = [role.name for role in member.roles]
+        embed.add_field(name="Status", value=str(member.status).title())
+        embed.add_field(name="On Mobile", value=member.is_on_mobile())
+        embed.add_field(name="User ID", value=member.id)
+        embed.add_field(name="Avatar", value=f"[Link]({member.avatar_url})")
+        embed.add_field(name="Joined Server", value=member.joined_at.replace(microsecond=0))
+        embed.add_field(name="Account Created", value=member.created_at.replace(microsecond=0))
+        embed.add_field(name="Roles", value=f"{len(roles)} roles" if len(", ".join(roles)) > 1000 else ", ".join(roles))
+        embed.set_thumbnail(url=member.avatar_url)
+        await ctx.send(embed=embed)
+
+    @commands.guild_only()
+    @commands.command(
+        description="Get some information about this server.",
+        usage="serverinfo",
+        aliases=["server"],
+    )
+    async def serverinfo(self, ctx):
+        guild = ctx.guild
+        roles = [role.name for role in guild.roles]
+        embed = discord.Embed(
+            title="Server Information",
+            color=self.bot.primary_colour,
+        )
+        embed.add_field(name="Name", value=guild.name)
+        embed.add_field(name="ID", value=guild.id)
+        embed.add_field(name="Owner", value=f"{guild.owner.name}#{guild.owner.discriminator}")
+        embed.add_field(name="Server Created", value=guild.created_at.replace(microsecond=0))
+        embed.add_field(name="Icon", value=f"[Link]({guild.icon_url})" if guild.icon_url is not None else "None")
+        embed.add_field(name="Channels", value=str(len(guild.text_channels) + len(guild.voice_channels)))
+        embed.add_field(name="Members", value=guild.member_count)
+        embed.add_field(name="Roles", value=f"{len(roles)} roles")
+        if guild.icon:
+            embed.set_thumbnail(url=guild.icon_url)
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
