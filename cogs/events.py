e42bb07ef9a243e93a46f0202083210139557a17
CHamburr <cenhan@outlook.com>
2019-05-25 20:30:36 +0800

    Remove DM for missing permissions

--- cogs/events.py
+++ cogs/events.py
@@ -148,29 +148,22 @@ class Events(commands.Cog):
     async def on_message(self, message):
         if message.author.bot:
             return
-        if message.content.startswith(f"<@{self.bot.user.id}>") or message.content.startswith(f"<@!{self.bot.user.id}>"):
+        prefix = get_guild_prefix(self.bot, message)
+        if message.guild and message.content.startswith(prefix):
+            permissions = message.channel.permissions_for(message.guild.me)
+            if permissions.send_messages is False:
+                return
+            elif permissions.send_messages is True and permissions.embed_links is False:
+                return await message.channel.send("The Embed Links permission is needed for basic commands to work.")
+        if message.content.startswith(f"<@{self.bot.user.id}>") or \
+           message.content.startswith(f"<@!{self.bot.user.id}>"):
             return await message.channel.send(
                 embed=discord.Embed(
-                    description=f"My prefix in this server is `{get_guild_prefix(self.bot, message)}`. Please use the "
-                                f"prefix instead. Run `{get_guild_prefix(self.bot, message)}help` for more information.",
+                    description=f"My prefix in this server is `{prefix}`. Please use the prefix instead. Run `{prefix} "
+                                f"help` for more information.",
                     color=self.bot.primary_colour,
                 )
             )
-        if message.guild:
-            permissions = message.channel.permissions_for(message.guild.me)
-            if permissions.send_messages is False:
-                try:
-                    return await message.author.send(
-                        embed=discord.Embed(
-                            description="The permissions `Send Messages` and `Embed Links` are needed for"
-                                        "basic commands to work.",
-                            color=self.bot.primary_colour,
-                        )
-                    )
-                except discord.Forbidden:
-                    pass
-            elif permissions.send_messages is True and permissions.embed_links is False:
-                return await message.channel.send("The Embed Links permission is needed for basic commands to work.")
         await self.bot.process_commands(message)
 
 
