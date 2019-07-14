fb6fbe3e87b0ed8d047925da0f098ffeba1e4e14
CHamburr <cenhan@outlook.com>
2019-07-14 15:13:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ default_prefix = "="
 activity = [
     f"DM to Contact Staff | {default_prefix}help",
     f"Invite Me - modmail.netlify.com | {default_prefix}help",
-    f"Premium Discount Code - GxiYRkEHTU | {default_prefix}help",
+    f"🎉 2000 Servers 🎉 | {default_prefix}help",
 ]
 
 # The very very main bot owner
