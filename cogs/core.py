7426897620e4c9117a28b3da068cc02bf798fb01
CHamburr <cenhan@outlook.com>
2020-05-08 11:07:07 +0800

    Add more logging

--- cogs/core.py
+++ cogs/core.py
@@ -128,7 +128,9 @@ class Core(commands.Cog):
                             log_url = f"https://discordtemplates.me/modmail-logs/{'-'.join(log_url)}"
                             embed.add_field(name="Message Logs", value=log_url, inline=False)
                             log.info(embed.to_dict())
-                            await msg.edit(embed=embed)
+                            msg2 = await msg.edit(embed=embed)
+                            log.info('new:')
+                            log.info(msg2.embeds[0].to_dict())
                             return
                         await channel.send(embed=embed)
                     except discord.Forbidden:
