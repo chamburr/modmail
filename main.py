63f3c8101a2489c292e482308c40284d10bb4dcf
CHamburr <cenhan@outlook.com>
2020-07-19 10:40:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def _get_guild_prefix(bot2, message):
 
 
 bot = ModMail(
-    fetch_offline_members=False,
+    fetch_offline_members=config.fetch_all_members,
     command_prefix=_get_guild_prefix,
     case_insensitive=True,
     help_command=None,
