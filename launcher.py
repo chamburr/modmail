b889420cffa765447aee3e84210095eb0f90a2c2
waterflamev8 <hi@pokeden.xyz>
2021-02-27 19:54:44 +0800

    Update

--- launcher.py
+++ launcher.py
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-"""
-Based on The IdleRPG Discord Bot
-Copyright (C) 2018-2021 Diniboy and Gelbpunkt
-Copyright (C) 2019-2021 CHamburr
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see https://www.gnu.org/licenses/.
-"""
-
-=======
->>>>>>> bruh cham why this so hard
-=======
->>>>>>> 54691043a8a98258d1a7ada96ff4177725830f40
 import asyncio
 import os
 import signal
