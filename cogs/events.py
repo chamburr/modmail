74cab0bb28ccb550cbb86268db68e796b9ddc79e
CHamburr <hi@chamburr.xyz>
2021-03-19 15:27:25 +0800

    perf: use redis pipelines

--- cogs/events.py
+++ cogs/events.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 import time
+import asyncio
 
 import discord
 
@@ -15,6 +16,13 @@ log = logging.getLogger(__name__)
 class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.pipe = self.bot.redis.pipeline()
+        self.bot.loop.create_task(self.execute_pipe())
+
+    async def execute_pipe(self):
+        while True:
+            await self.pipe.execute()
+            await asyncio.sleep(0.5)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -26,42 +34,41 @@ class Events(commands.Cog):
             if int(message["d"]["user"]["id"]) == self.bot.id:
                 return
 
-            await self.bot.state.sadd(f"user:{message['d']['user']['id']}", message["d"]["guild_id"])
-            await self.bot.state.sadd("user_keys", f"user:{message['d']['user']['id']}")
+            await self.bot.state.sadd(f"user:{message['d']['user']['id']}", message["d"]["guild_id"], pipe=self.pipe)
+            await self.bot.state.sadd("user_keys", f"user:{message['d']['user']['id']}", pipe=self.pipe)
         elif message["t"] == "GUILD_MEMBER_ADD":
             if int(message["d"]["user"]["id"]) == self.bot.id:
-                await self.bot.state.set(f"member:{message['d']['guild_id']}:{self.bot.id}", message["d"])
+                await self.bot.state.set(
+                    f"member:{message['d']['guild_id']}:{self.bot.id}", message["d"], pipe=self.pipe
+                )
                 return
 
-            await self.bot.state.sadd(f"user:{message['d']['user']['id']}", message["d"]["guild_id"])
-            await self.bot.state.sadd("user_keys", f"user:{message['d']['user']['id']}")
+            await self.bot.state.sadd(f"user:{message['d']['user']['id']}", message["d"]["guild_id"], pipe=self.pipe)
+            await self.bot.state.sadd("user_keys", f"user:{message['d']['user']['id']}", pipe=self.pipe)
         elif message["t"] == "GUILD_MEMBER_REMOVE":
             if int(message["d"]["user"]["id"]) == self.bot.id:
-                await self.bot.state.delete(f"member:{message['d']['guild_id']}:{self.bot.id}")
+                await self.bot.state.delete(f"member:{message['d']['guild_id']}:{self.bot.id}", pipe=self.pipe)
                 return
 
-            await self.bot.state.srem(f"user:{message['d']['user']['id']}", message["d"]["guild_id"])
-
-            if await self.bot.state.scard(f"user:{message['d']['user']['id']}") == 0:
-                await self.bot.state.srem("user_keys", f"user:{message['d']['user']['id']}")
+            await self.bot.state.srem(f"user:{message['d']['user']['id']}", message["d"]["guild_id"], pipe=self.pipe)
         elif message["t"] == "GUILD_MEMBER_UPDATE":
             if int(message["d"]["user"]["id"]) == self.bot.id:
                 member = await self.bot.state.get(f"member:{message['d']['guild_id']}:{self.bot.id}")
                 if member:
                     member["roles"] = message["d"]["roles"]
-                    await self.bot.state.set(f"member:{message['d']['guild_id']}:{self.bot.id}", member)
+                    await self.bot.state.set(f"member:{message['d']['guild_id']}:{self.bot.id}", member, pipe=self.pipe)
                 return
 
-            await self.bot.state.sadd(f"user:{message['d']['user']['id']}", message["d"]["guild_id"])
-            await self.bot.state.sadd("user_keys", f"user:{message['d']['user']['id']}")
+            await self.bot.state.sadd(f"user:{message['d']['user']['id']}", message["d"]["guild_id"], pipe=self.pipe)
+            await self.bot.state.sadd("user_keys", f"user:{message['d']['user']['id']}", pipe=self.pipe)
         elif message["t"] == "GUILD_CREATE":
             for member in message["d"]["members"]:
                 if int(member["user"]["id"]) == self.bot.id:
-                    await self.bot.state.set(f"member:{message['d']['id']}:{self.bot.id}", member)
+                    await self.bot.state.set(f"member:{message['d']['id']}:{self.bot.id}", member, pipe=self.pipe)
                     continue
 
-                await self.bot.state.sadd(f"user:{member['user']['id']}", message["d"]["id"])
-                await self.bot.state.sadd("user_keys", f"user:{member['user']['id']}")
+                await self.bot.state.sadd(f"user:{member['user']['id']}", message["d"]["id"], pipe=self.pipe)
+                await self.bot.state.sadd("user_keys", f"user:{member['user']['id']}", pipe=self.pipe)
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
