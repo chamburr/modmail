6c915d44f1341bc65e1e5fcda5349e2e52ff6e75
dlsnyder8 <51924672+dlsnyder8@users.noreply.github.com>
2021-06-03 05:57:49 -0400

    chore: improve wording for login message (#84)

--- utils/tools.py
+++ utils/tools.py
@@ -102,7 +102,8 @@ async def select_guild(bot, message, msg):
     user_guilds = await get_user_guilds(bot, message.author)
     if user_guilds is None:
         embed = Embed(
-            f"Please [click here]({bot.config.BASE_URI}/login?redirect=/authorized) to login. "
+            f"Please [click here]({bot.config.BASE_URI}/login?redirect=/authorized) to verify. "
+            "This will allow the bot to see your servers and is required for the bot to function. "
             "Then, close the page and return back here."
         )
         await msg.edit(embed)
