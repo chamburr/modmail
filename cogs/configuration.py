67b5631c17f86186ed0a11fa8b01dc6ed2c5177e
Han Cen <hi@chamburr.com>
2022-04-26 10:14:31 +0800

    Update configuration.py

--- cogs/configuration.py
+++ cogs/configuration.py
@@ -305,35 +305,27 @@ class Configuration(commands.Cog):
 
         await ctx.send(Embed("The channel is created successfully."))
 
-
-
     @checks.in_database()
-    @checks.is_premium()
-    @commands.guild_only()
     @checks.has_permissions(administrator=True)
+    @commands.guild_only()
     @commands.command(
-        description="Toggle the bot to only send messages in a ticket if the `=reply` or `=areply` commands are used",
-        aliases=["commandonly"],
-        usage="commandrequired"
+        description="Toggle whether commands are required to reply to a ticket.",
+        aliases=["commandrequired"],
+        usage="commandonly",
     )
-    async def commandrequired(self,ctx):
+    async def commandonly(self, ctx):
         data = await tools.get_data(self.bot, ctx.guild.id)
 
-        if data[12]:
-            async with self.bot.pool.acquire() as conn:
-                await conn.execute("UPDATE data SET commandrequired=$1 WHERE guild=$2",False,ctx.guild.id)
+        async with self.bot.pool.acquire() as conn:
+            await conn.execute(
+                "UPDATE data SET commandonly=$1 WHERE guild=$2",
+                True if data[12] is False else False,
+                ctx.guild.id,
+            )
 
-            await ctx.send(Embed(f"Commands are no longer required to send messages"))
-            return
-
-        else:
-            async with self.bot.pool.acquire() as conn:
-                await conn.execute("UPDATE data SET commandrequired=$1 WHERE guild=$2",True,ctx.guild.id)
-
-            await ctx.send(Embed(f"Commands are now required to send messages."))
-            return
-
-
+        await ctx.send(
+            Embed(f"Command only mode is {'enabled' if data[12] is False else 'disabled'}.")
+        )
 
     @checks.in_database()
     @checks.is_premium()
@@ -454,9 +446,9 @@ class Configuration(commands.Cog):
         )
         embed.add_field("Advanced Logging", "Enabled" if data[7] is True else "Disabled")
         embed.add_field("Anonymous Messaging", "Enabled" if data[10] is True else "Disabled")
-        embed.add_field("Command Required Mode","Enabled" if data[12] is True else "Disabled")
+        embed.add_field("Command Only Mode","Enabled" if data[12] is True else "Disabled")
         embed.add_field("Greeting Message", "*Not set*" if greeting is None else greeting, False)
-        embed.add_field("Closing message", "*Not set*" if closing is None else closing, False)
+        embed.add_field("Closing Message", "*Not set*" if closing is None else closing, False)
 
         await ctx.send(embed)
 
