f7b5f4526e7fd8c339d73bb4b9bf214cefa11038
Han Cen <hi@chamburr.com>
2022-12-14 21:13:31 +0800

    chore: remove plausible

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -56,15 +56,6 @@ export default {
       { rel: 'manifest', href: '/site.webmanifest' },
       { rel: 'canonical', href: `${process.env.BASE_URI}` },
     ],
-    ...(!isDevelopment() && {
-      script: [
-        {
-          src: 'https://plausible.chamburr.xyz/js/script.js',
-          defer: true,
-          'data-domain': `${process.env.BASE_URI.split('/')[2]}`,
-        },
-      ],
-    }),
   },
 
   build: {
