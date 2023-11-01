ae20440625c355180ad53b336eb9a3927c7a8ebb
Han Cen <hi@chamburr.com>
2023-11-01 19:45:46 +0800

    fix: update docker entrypoint script

--- docker/bot/entrypoint.sh
+++ docker/bot/entrypoint.sh
@@ -9,8 +9,12 @@ start() {
         /app/scripts/database.sh migrate
     fi
 
-    while [[ $(echo "EXISTS bot_user" | nc -z redis 6379) =~ "0" ]]; do
+    while [[ ! $(echo "EXISTS bot_user" | nc -q1 redis 6379 2>&1) =~ ":1" ]]; do
        sleep 1
+    done
+
+    while [[ ! $(echo "12345\n" | nc -q1 rabbitmq 5672 2>&1 | tr -d "\0") =~ "AMQP" ]]; do
+        sleep 1;
     done
 
     python3 -u main.py
