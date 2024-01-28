9a3e5e4c9ca0013f0abbeb419859da79a2b1984f
Han Cen <hi@chamburr.com>
2024-01-28 20:29:55 +0800

    fix: use environment variables in entrypoint

--- docker/bot/entrypoint.sh
+++ docker/bot/entrypoint.sh
@@ -9,11 +9,11 @@ start() {
         /app/scripts/database.sh migrate
     fi
 
-    while [[ ! $(echo "EXISTS bot_user" | nc -q1 redis 6379 2>&1) =~ ":1" ]]; do
+    while [[ ! $(echo "EXISTS bot_user" | nc -q1 "$REDIS_HOST" 6379 2>&1) =~ ":1" ]]; do
        sleep 1
     done
 
-    while [[ ! $(echo "12345\n" | nc -q1 rabbitmq 5672 2>&1 | tr -d "\0") =~ "AMQP" ]]; do
+    while [[ ! $(echo "12345\n" | nc -q1 "$RABBIT_HOST" 5672 2>&1 | tr -d "\0") =~ "AMQP" ]]; do
         sleep 1;
     done
 
