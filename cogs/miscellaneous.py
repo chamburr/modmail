19d622a810d153cf2276653292bff3d1681d63ca
Scyye <scyyebiz@gmail.com>
2024-04-26 10:14:29 -0400

    style(bot): do it the right way :eye_roll:

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -30,11 +30,15 @@ class Miscellaneous(commands.Cog):
         embed.add_field("User", str(member), False)
         embed.add_field(
             "Allowed",
-            "\n".join([tools.perm_format(name) for name, value in permissions if value]),
+            "\n".join(
+                [tools.perm_format(name) for name, value in permissions if value]
+            ),
         )
         embed.add_field(
             "Denied",
-            "\n".join([tools.perm_format(name) for name, value in permissions if not value]),
+            "\n".join(
+                [tools.perm_format(name) for name, value in permissions if not value]
+            ),
         )
 
         await ctx.send(embed)
@@ -57,14 +61,19 @@ class Miscellaneous(commands.Cog):
         embed.add_field("Name", str(member))
         embed.add_field("ID", member.id)
         embed.add_field("Nickname", member.nick if member.nick else "*Not set*")
-        embed.add_field("Avatar", f"[Link]({member.avatar_url_as(static_format='png')})")
+        embed.add_field(
+            "Avatar", f"[Link]({member.avatar_url_as(static_format='png')})"
+        )
         embed.add_field(
             "Joined Server",
-            f"<t:{round(member.joined_at.replace(microsecond=0)
-                        .timestamp())}:R>" if member.joined_at else "Unknown",
+            f"<t:{round(member.joined_at.replace(microsecond=0).timestamp())}:R>"
+            if member.joined_at
+            else "Unknown",
         )
-        embed.add_field("Account Created", f"<t:{round(member.created_at.replace(microsecond=0)
-                                                       .timestamp())}:R>")
+        embed.add_field(
+            "Account Created",
+            f"<t:{round(member.created_at.replace(microsecond=0).timestamp())}:R>",
+        )
         embed.add_field(
             "Roles",
             f"{len(roles)} roles" if len(", ".join(roles)) > 1000 else ", ".join(roles),
@@ -85,13 +94,19 @@ class Miscellaneous(commands.Cog):
         embed = Embed(title="Server Information")
         embed.add_field("Name", guild.name)
         embed.add_field("ID", guild.id)
-        embed.add_field("Owner", f"<@{guild.owner_id}>" if guild.owner_id else "Unknown")
+        embed.add_field(
+            "Owner", f"<@{guild.owner_id}>" if guild.owner_id else "Unknown"
+        )
         embed.add_field(
             "Icon",
-            f"[Link]({guild.icon_url_as(static_format='png')})" if guild.icon else "*Not set*",
+            f"[Link]({guild.icon_url_as(static_format='png')})"
+            if guild.icon
+            else "*Not set*",
         )
-        embed.add_field("Server Created", f"<t:{round(guild.created_at.replace(microsecond=0)
-                                                      .timestamp())}:R>")
+        embed.add_field(
+            "Server Created",
+            f"<t:{round(guild.created_at.replace(microsecond=0).timestamp())}:R>",
+        )
         embed.add_field("Members", guild.member_count)
         embed.add_field("Channels", str(len(await guild.channels())))
         embed.add_field("Roles", str(len(await guild.roles())))
