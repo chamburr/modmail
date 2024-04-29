6dddcf536f7a951f71a184a676fc797cb907d973
DangerCode <157859533+TornikeCodes@users.noreply.github.com>
2024-04-29 11:36:54 -0400

    Added an option where it returns "exclude muted members" with the data

--- utils/tools.py
+++ utils/tools.py
@@ -181,7 +181,7 @@ async def get_data(bot, guild):
             return res
 
         return await conn.fetchrow(
-            "INSERT INTO data VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) "
+            "INSERT INTO data VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) "
             "RETURNING *",
             guild,
             None,
@@ -196,6 +196,7 @@ async def get_data(bot, guild):
             False,
             False,
             None,
+            False,
         )
 
 
