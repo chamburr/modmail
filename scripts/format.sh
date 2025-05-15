5ef7d8c9b5b93767e48ce9dd0ce9529456827580
Han Cen <hi@chamburr.com>
2025-05-15 16:38:07 +0800

    fix: fix prettier args

--- scripts/format.sh
+++ scripts/format.sh
@@ -6,7 +6,7 @@ echo "Formatting Rust..."
 cd "$WORKDIR/web" && cargo fmt
 
 echo "Formatting Node..."
-cd "$WORKDIR/web" && yarn --silent format --loglevel warn
+cd "$WORKDIR/web" && yarn --silent format --log-level warn
 
 echo "Formatting Python..."
 cd "$WORKDIR" && isort . && black . --line-length 100
